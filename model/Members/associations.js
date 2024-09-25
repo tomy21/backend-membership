6f8c46d2e8f7490499f079a26c2fc9d685eca491
tomyItSky <tomy.agung@skyparking.co.id>
2024-09-25 11:08:26 +0700

    fixingLocationCode

--- model/Members/associations.js
+++ model/Members/associations.js
@@ -2,15 +2,14 @@ import { Location } from "../../model/Master/RefLocation.js";
 import MemberProduct from "./ProductMember.js";
 
 export function initAssociations() {
-  MemberProduct.belongsTo(Location, {
-    foreignKey: "LocationCode",
-    targetKey: "Code",
-    as: "Location",
-  });
-
-  Location.hasMany(MemberProduct, {
-    foreignKey: "LocationCode",
-    sourceKey: "Code",
-    as: "Products",
-  });
+  // MemberProduct.belongsTo(Location, {
+  //   foreignKey: "LocationCode",
+  //   targetKey: "Code",
+  //   as: "Location",
+  // });
+  // Location.hasMany(MemberProduct, {
+  //   foreignKey: "LocationCode",
+  //   sourceKey: "Code",
+  //   as: "Products",
+  // });
 }

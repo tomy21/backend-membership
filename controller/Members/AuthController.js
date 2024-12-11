ad489987c672700341ac1f3a97a080642c988e50
tomy21 <tomy21.agung@gmail.com>
2024-12-11 16:15:32 +0700

    .

--- controller/Members/AuthController.js
+++ controller/Members/AuthController.js
@@ -38,7 +38,7 @@ const createSendToken = (user, statusCode, res, rememberMe) => {
   const token = signToken(user, rememberMe);
 
   res.cookie("refreshToken", token, {
-    httpOnly: true,
+    httpOnly: false,
     secure: process.env.NODE_ENV === "production",
     expires: new Date(Date.now() + (rememberMe ? 30 : 1) * 24 * 60 * 60 * 1000),
     sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax",

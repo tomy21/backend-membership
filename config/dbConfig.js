805ec92f78bd16bd317efe661adbda51d5d3ab36
tomy21 <tomy21.agung@gmail.com>
2024-12-11 11:40:55 +0700

    .

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
-dotenv.config({ path: ".env.membershipV2" });
+dotenv.config();
 
 const db = new Sequelize(
   process.env.DB_NAME,

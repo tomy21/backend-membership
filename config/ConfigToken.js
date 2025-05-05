a40f3afe818429681f0cfc374dbb33281074d47e
tomy21 <tomy21.agung@gmail.com>
2025-05-05 17:03:19 +0700

    update

--- config/ConfigToken.js
+++ config/ConfigToken.js
@@ -25,10 +25,10 @@ export const createSendToken = (user, statusCode, res, rememberMe) => {
   const token = signToken(user, rememberMe);
 
   res.cookie("refreshToken", token, {
-    httpOnly: false,
+    httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     expires: new Date(Date.now() + (rememberMe ? 30 : 1) * 24 * 60 * 60 * 1000),
-    // sameSite: "Lax",
+    sameSite: "Lax",
   });
 
   const response = {

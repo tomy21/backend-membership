5d04c4ca6524cdc0266e041393f58a9dec049074
tomy21 <tomy21.agung@gmail.com>
2025-05-08 11:37:25 +0700

    updated

--- config/ConfigToken.js
+++ config/ConfigToken.js
@@ -30,6 +30,7 @@ export const createSendToken = (user, statusCode, res, rememberMe) => {
     secure: process.env.NODE_ENV === "production",
     expires: new Date(Date.now() + (rememberMe ? 30 : 1) * 24 * 60 * 60 * 1000),
     sameSite: "none",
+    domain: ".skyparking.online",
   });
 
   const response = {

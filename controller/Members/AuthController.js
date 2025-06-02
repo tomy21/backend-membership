d9e797987b259a58c64a7040733290874704d105
tomy21 <tomy21.agung@gmail.com>
2025-06-02 22:54:33 +0700

    update

--- controller/Members/AuthController.js
+++ controller/Members/AuthController.js
@@ -483,9 +483,7 @@ export const register = async (req, res) => {
     const activationToken = newUser.createActivationToken(referralUrl);
     await newUser.save({ validate: false });
 
-    const activationURL = `${req.protocol}://${req.get(
-      "host"
-    )}/v01/member/api/auth/activate/${activationToken}`;
+    const activationURL = `${referralUrl}/v01/member/api/auth/activate/${activationToken}`;
     const to = newUser.email;
     const subject = "Welcome to SKY PARKING - Activate Your Account";
     const html = `

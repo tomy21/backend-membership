db3b87d9308442f5e32fc2e0596a4a73f5ff9718
tomy21 <tomy21.agung@gmail.com>
2025-06-17 08:47:23 +0700

    updated

--- controller/Members/AuthController.js
+++ controller/Members/AuthController.js
@@ -558,8 +558,6 @@ export const activateAccount = async (req, res) => {
       },
     });
 
-    console.log(user);
-
     const userCMS = await UserCMS.findOne({
       where: {
         active_token: hashedToken,
@@ -567,17 +565,14 @@ export const activateAccount = async (req, res) => {
       },
     });
 
-    if (!user && !userCMS) {
-      return res.status(400).json({
-        status: "fail",
-        message: "Token is invalid or has expired",
-      });
-    }
-
     const allowedDomains =
       `${req.protocol}://${req.get("host")}` === "http://localhost:3008"
         ? "http://localhost:3000"
         : `https://membership.skyparking.online`;
+
+    if (!user && !userCMS) {
+      res.redirect(`${allowedDomains}/request-token`);
+    }
 
     if (user) {
       user.is_active = 1;

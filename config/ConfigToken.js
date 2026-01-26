e7e6288c3f3b1eca5ee12bf9f176be3ba3c9dec8
Tomy Agung <tomy21.agung@gmail.com>
2026-01-26 16:17:26 +0700

    perbaikan

--- config/ConfigToken.js
+++ config/ConfigToken.js
@@ -30,7 +30,7 @@ export const createSendToken = (user, statusCode, res, rememberMe) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     expires: new Date(Date.now() + (rememberMe ? 30 : 1) * 24 * 60 * 60 * 1000),
-    sameSite: "strict",
+    sameSite: "None",
     domain: ".skyparking.online",
   });
 
